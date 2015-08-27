f8110597b56aba5ed7c79e1d86d90ad696678ae9
CFWLoader <cfwloader@gmail.com>
2015-08-27 16:37:45 +0800

    Add a magic.

---
+++ Inside-the-C++-Object-Model/Static-Member-Function/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class HasFunctionMember
+{
+public:
+    int fun()
+    {
+        cout << "This is a non-static member function." << endl;
+    }
+private:
+};
+
+int main()
+{
+    // In the age of C++ class hasn't static member function, programmers simulate static member function in this way.
+    ((HasFunctionMember*)0)->fun();
+
+    // New C++ style cast.
+    static_cast<HasFunctionMember*>(0)->fun();
+
+    return 0;
+}
