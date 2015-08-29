e436526d2eea7181e190dd5cc64d5ebb04b44fad
CFWLoader <cfwloader@gmail.com>
2015-08-29 11:11:36 +0800

    Leave for school tomorrow.

--- Inside-the-C++-Object-Model/Memory-Layout-detecting/main.cpp
+++ Inside-the-C++-Object-Model/Memory-Layout-detecting/main.cpp
@@ -4,7 +4,16 @@
 
 using namespace std;
 
+int simpleTest();
+
+int MI_Test();
+
 int main()
+{
+    return MI_Test();
+}
+
+int simpleTest()
 {
     printf("mem_1 address: %p.\n", &SampleClass::mem_1);
     printf("mem_2 address: %p.\n", &SampleClass::mem_2);
@@ -13,19 +22,42 @@ int main()
 
     SampleClass sampleClass;
 
-    char* p = (char*)&sampleClass;
+    char *p = (char *) &sampleClass;
 
     //  Obtaining virtual pointer.
-    int** vptr = (int**)p;
+    int **vptr = (int **) p;
 
     //  Obtaining virtual table of virtual functions via vptr.
-    int** vtbl = (int**)(*vptr);
+    int **vtbl = (int **) (*vptr);
 
     typedef void (*FunPtr)();
 
     FunPtr func = (FunPtr) vtbl[0];
 
+    printf("member funtion--print address: %p.\n", vtbl[0]);
+
     func();
+
+    return 0;
+}
+
+int MI_Test()
+{
+    Derived d;
+    char *p = (char *) &d;
+    visitVtbl((int **) *(int **) p, 3);
+    p += sizeof(int **);
+
+    cout << *(int64_t *) p << endl;
+    p += sizeof(int64_t);
+
+    visitVtbl((int **) *(int **) p, 3);
+    p += sizeof(int **);
+
+    cout << *(int64_t *) p << endl;
+    p += sizeof(int64_t);
+
+    cout << *(int *) p << endl;
 
     return 0;
 }
