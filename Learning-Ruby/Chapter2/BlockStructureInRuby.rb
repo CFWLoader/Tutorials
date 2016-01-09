a36f0c355efef7971c5aadf208c9489cf23b0310
cfwloader <cfwloader@yahoo.com>
2016-01-09 21:56:29 +0800

    Learnt some in chapter 2.

---
+++ Learning-Ruby/Chapter2/BlockStructureInRuby.rb
@@ -0,0 +1,12 @@
+# Single line block code.
+
+3.times { print "Ruby!" }
+
+print "\n"
+
+# Multi lines block codes.
+
+1.upto(10) do |x|
+  print x
+  print ' '
+end
---
+++ Learning-Ruby/Chapter2/CommentsInRuby.rb
@@ -0,0 +1,8 @@
+# Single line comment.
+
+y = /# This is a comment too./ # Regular Expression.
+
+=begin
+  Multi lines comments is donated in keyword
+  "=being" and "=end".
+=end
