4d320b5a3d96487955bc7f595b07b80ed352d58a
CFWLoader <cfwloader@gmail.com>
2016-01-27 12:00:04 +0800

    Finish RoR startup tutorals.

--- Ruby-On-Rails/blog/app/controllers/articles_controller.rb
+++ Ruby-On-Rails/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def new
     @article = Article.new
   end
