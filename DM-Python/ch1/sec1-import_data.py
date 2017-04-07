7a9f24f14870435ef1f48eef77dc9b07c6cb4d54
CFWLoader <cfwloader@gmail.com>
2017-04-07 10:54:34 +0800

    Fit urllib to py3 version.

--- DM-Python/ch1/sec1-import_data.py
+++ DM-Python/ch1/sec1-import_data.py
@@ -1,13 +1,15 @@
 __author__ = 'CFWLoader'
 
-import urllib2
+# import urllib2
+import urllib.request as reqs
 
 url = 'http://aima.cs.berkeley.edu/data/iris.csv'
 
-u = urllib2.urlopen(url)
+# u = urllib2.urlopen(url)
+u = reqs.urlopen(url)
 
 localFile = open('iris.csv', 'w')
 
-localFile.write(u.read())
+localFile.write(bytes.decode(u.read()))
 
 localFile.close()
