21b64559c3946f84a648d27ea8faa87f5b13bd33
CFWLoader <cfwloader@gmail.com>
2017-05-06 11:28:21 +0800

    Fibonacci squence and Count change.

---
+++ sicp/ch1/fib_squence.scm
@@ -0,0 +1,13 @@
+(define (fib-r n)
+	(cond 	((= n 0) 0)
+		((= n 1) 1)
+		(else (+ (fib-r (- n 1))
+			 (fib-r (- n 2))))))
+
+(define (fib n)
+	(fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+	(if (= count 0)
+		b
+		(fib-iter (+ a b) a (- count 1))))
