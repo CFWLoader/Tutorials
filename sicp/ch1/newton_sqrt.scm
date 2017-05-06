e7d8cd9aa27ac054659ea313ca31dc2d859f0f8f
CFWLoader <cfwloader@gmail.com>
2017-05-06 10:56:58 +0800

    Newton method for square root.

--- sicp/ch1/newton_sqrt.scm
+++ sicp/ch1/newton_sqrt.scm
@@ -1,5 +1,8 @@
 #lang racket
 
+(define (square x)
+	(* x x))
+
 (define (sqrt-iter guess x)
 	(if (good-enough? guess x)
 		guess
@@ -8,3 +11,14 @@
 
 (define (improve guess x)
 	(average guess (/ x guess)))
+
+(define (average x y)
+	(/ (+ x y) 2))
+
+(define (good-enough? guess x)
+	(< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+	(sqrt-iter 1.0 x))
+
+(sqrt 9)
