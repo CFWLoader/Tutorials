b367f026bc0d9a5841641261b02bfa7a7ce338f9
CFWLoader <cfwloader@gmail.com>
2017-05-17 16:34:16 +0800

    Checkpoint.

---
+++ sicp/ch2/hierarchy_structure.scm
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (length items)
+	(if (null? items)
+		0
+		(+ 1 (length (cdr items))))
+)
+
+(define (count-leaves x)
+	(cond 	((null? x) 0)
+			((not (pair? x)) 1)
+			(else (+ 	(count-leaves (car x))
+						(count-leaves (cdr x))))))
+
+(define x (cons (list 1 2) (list 3 4)))
+
+(length x)
+
+(count-leaves x)
+
+(list x x)
+
+(length (list x x))
+
+(count-leaves (list x x))
