6d461afd3e0920ef0e830514a7dbdb65de8a6e2d
CFWLoader <cfwloader@gmail.com>
2017-06-05 16:06:26 +0800

    Procedure abstraction.

---
+++ sicp/ch1/proc_as_ret_val.rb
@@ -0,0 +1,59 @@
+require './fixed_point.rb'
+
+def average_damp f
+
+  lambda{|x| (x + f.call(x)) / 2.0}
+
+end
+
+
+def average_damp_ruby x, f
+
+  yield (x + f.call(x)) / 2.0
+
+end
+
+
+def sqrt x
+
+  fixed_point average_damp(lambda{|y| x / y}), 1.0
+
+end
+
+
+def cube_root x
+
+  fixed_point average_damp(lambda{|y| x / (y ** 2)}), 1.0
+
+end
+
+DX = 0.00001
+
+# derivative
+def deriv g
+
+  lambda{|x| (g.call(x + DX) - g.call(x)) / DX}
+
+end
+
+
+def newton_transform g
+
+  
+
+end
+
+
+if $0 == __FILE__
+
+  p average_damp(lambda{|x| x ** 2}).call 10
+
+  average_damp_ruby(10, lambda{|x| x ** 2}) {|x| p x}
+
+  p sqrt 2
+
+  p cube_root 27
+
+  p deriv(lambda{|x| x ** 3}).call 5
+
+end
