4531d5e7daf78087611aca4ca4bc923f61c00dae
CFWLoader <cfwloader@gmail.com>
2017-06-14 11:40:05 +0800

    I need binding.

--- sicp/ch3/circuit.rb
+++ sicp/ch3/circuit.rb
@@ -1,8 +1,30 @@
 class Wire
 
-  def initialize symbol
+  def initialize symbol, init_signal = false
 
     @symbol = symbol
+
+    @signal = init_signal
+
+  end
+
+  # Lazy evaluation.
+  # SICP original codes is constructing a callback fun.
+  def get_signal
+
+    @action.nil? ? @signal : @action
+
+  end
+
+  def set_signal! new_value
+
+    @signal = new_value
+
+  end
+
+  def add_action! proc, **args
+
+    @action = proc.call args
 
   end
 
@@ -50,6 +72,22 @@ class Inverter < Gate
 
     @output = output_wire
 
+    invert_input @input, @output
+
+  end
+
+  def invert_input input, output
+
+    expr = lambda {|i| i.get_signal}
+
+    output.add_action! expr, ie: input
+
+  end
+
+  def get_output
+
+    @output.get_signal
+
   end
 
 end
@@ -59,17 +97,17 @@ class HalfAdder
 
   def initialize a, b, s, c
 
-    @d = Wire.new :d
+    d = Wire.new :d
 
-    @e = Wire.new :e
+    e = Wire.new :e
 
-    @or_gate = OrGate.new a, b, @d
+    @or_gate = OrGate.new a, b, d
 
     @and_gate1 = AndGate.new a, b, c
 
-    @inverter = Inverter.new c, @e
+    @inverter = Inverter.new c, e
 
-    @and_gate2 = AndGate.new @d, @e, s
+    @and_gate2 = AndGate.new d, e, s
 
   end
 
@@ -80,11 +118,37 @@ class FullAdder
 
   def initialize a, b, c_in, sum, c_out
 
+    s = Wire.new :s
+
+    c1 = Wire.new :c1
+
+    c2 = Wire.new :c2
+
+    half_adder1 = HalfAdder.new b, c_in, s, c1
+
+    half_adder2 = HalfAdder.new a, s, sum, c2
+
+    or_gate = OrGate.new c1, c2, c_out
+
   end
 
 end
 
 
 if $0 == __FILE__
+
+  i = Wire.new :in1
+
+  o = Wire.new :out1
+
+  inverter = Inverter.new i, o
+
+  i.set_signal! 0
+
+  puts inverter.get_output
+
+  i.set_signal! 1
+
+  puts inverter.get_output
 
 end
