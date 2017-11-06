02013aa90ccb0193e8a5daf96c88ae9cfa39a452
Evan Huang <cfwloader@gmail.com>
2017-11-06 11:39:50 +0800

    Qt is temporary deprecated.

--- Qt/Qt-Exploring/DemoCases.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEMOCASES_H
-#define DEMOCASES_H
-
-int demo1(int argc, char* argv[]);
-int demo2(int argc, char* argv[]);
-int demo3(int argc, char* argv[]);
-int demo4(int argc, char* argv[]);
-
-#endif // DEMOCASES_H
--- Qt/README.md
+++
@@ -1,4 +0,0 @@
-# Learning Qt
-
-### Because Mr.Deng likes GUI and GUI can make you getting high marks in his exam.
-### So it's necessary to learn coding GUI.
