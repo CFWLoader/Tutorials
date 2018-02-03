156190d0b4c6ee898b3160ccafab77a60433312a
cfwloader <cfwloader@gmail.com>
2018-02-03 16:25:37 +0800

    Save something.

--- R/HelloWorld/Operators.R
+++ R/HelloWorld/Operators.R
@@ -1,3 +1,9 @@
-v <- c(2, 3.555555, 676)
+v1 <- c(2, 3.555555, 676)
 
-t <- c(8, 54, 879)
+v2 <- c(8, 54, 879)
+
+# Plus each corresponding items in vector.
+print(v1 + v2)
+
+# Minus
+print(v2 - v1)
