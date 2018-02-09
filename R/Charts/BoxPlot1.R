c8255e804cb846279793a5cd5e639a4372df0a43
CFWLoader <cfwloader@gmail.com>
2018-02-09 13:31:37 +0800

    Checkpoint.

--- R/Charts/BoxPlot1.R
+++ R/Charts/BoxPlot1.R
@@ -6,3 +6,5 @@
 # boxplot(x, data, notch, varwidth, names, main)
 # x - vector or formula.
 # data - data or dataframe.
+# notch - TRUE/FALSE value, allowing notch or not.
+# varwidth - TRUE/FALSE value,
