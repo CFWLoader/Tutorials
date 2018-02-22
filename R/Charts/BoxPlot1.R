abcd275147365e15793f9799892275b2d7aff988
CFWLoader <cfwloader@gmail.com>
2018-02-22 11:15:00 +0800

    Minor copy.

--- R/Charts/BoxPlot1.R
+++ R/Charts/BoxPlot1.R
@@ -7,4 +7,21 @@
 # x - vector or formula.
 # data - data or dataframe.
 # notch - TRUE/FALSE value, allowing notch or not.
-# varwidth - TRUE/FALSE value,
+# varwidth - TRUE/FALSE value, TRUE for scalable frame.
+# names - Group label for box charts.
+# main - title for Charts.
+
+# Load data input.
+input <- mtcars[,c('mpg','cyl')]
+
+# print(head(input))
+
+# Give the chart file a name.
+png(file = "boxplot1.png")
+
+# Plot the chart.
+boxplot(mpg ~ cyl, data = mtcars, xlab = "Number of Cylinders",
+   ylab = "Miles Per Gallon", main = "Mileage Data")
+
+# Save the file.
+dev.off()
