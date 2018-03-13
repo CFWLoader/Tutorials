03fd11aa6a2391eb18095ba8e7de928364fd8852
cfwloader <cfwloader@gmail.com>
2018-03-13 19:40:10 +0800

    Save a demo.

---
+++ R/StaticsTest/STest.R
@@ -0,0 +1,10 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Evan
+# Created on: 2018/3/13
+
+library(SuppDists)
+library(kSamples)
+
+print(runif(50))
+# print(ad.test(runif(50), rnorm(30)))
